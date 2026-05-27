5b35be498a3af57ee5200a6e99bb815c1faf6646
Lars B <brinkmann66@gmx.de>
2026-05-27 10:21:16 +0200

    more inference

--- src/PP_parser/PPStructureV3_parser.py
+++ src/PP_parser/PPStructureV3_parser.py
@@ -1,7 +1,7 @@
 from paddleocr import PPStructureV3, PaddleOCRVL
 from PP_vis import save_ppstructure_visualizations
 
-img_path = "data/Lieferschein-Beispiel.png"
+img_path = r"data\datasets\cmr_dachser_20260520\train\images\0ff01432-aefe-45a4-8980-e941151f737d_CMR_page_1.jpg"
 # pipeline = PPStructureV3(
 #     lang="de",  # or "en"
 #     use_doc_orientation_classify=True,
@@ -10,16 +10,19 @@ img_path = "data/Lieferschein-Beispiel.png"
 # )
 
 pipeline = PaddleOCRVL(
-    device="cpu",
-    cpu_threads=8,
+    pipeline_version="v1.5",
+    device="gpu",
+    # cpu_threads=8,
     use_layout_detection=True,   # lighter first test
+    merge_layout_blocks=False,
     use_queues=False              # simpler for a single image
 )
 
 results = pipeline.predict(
     img_path,
-    max_pixels=512 * 512,
-    max_new_tokens=256
+    use_doc_preprocessor=True,
+    # max_pixels=512 * 512,
+    # max_new_tokens=256
 )
 
 save_ppstructure_visualizations(results, "output/vis")
